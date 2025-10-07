ed9afb22a2f7871d78f8f0cb47760ade758f41c8
Savin Angel-Mario <hello@m4.rocks>
2025-10-07 18:24:08 +0000

    Test without /:path

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
 	vercel: {
 		distContainsOnlyStatic: false,
 		rewrites: [
-			{ source: "/:path((?!api/).*)", destination: "/:path.html" },
+			// { source: "/:path", destination: "/:path.html" },
 			{ source: "/(.*)", destination: "/api/handler" }
 		],
 		additionalEndpoints: [
