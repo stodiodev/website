8611f2525e4684f7a90a0a3d97ae728e9db472d5
Savin Angel-Mario <hello@m4.rocks>
2025-10-07 17:38:11 +0000

    Added return to the handler

--- vercel-endpoint/handler.js
+++ vercel-endpoint/handler.js
@@ -1,4 +1,9 @@
+/**
+ * @param {import("@vercel/node").VercelRequest} req - The incoming request object.
+ * @returns {Promise<import("@vercel/node").VercelResponse>} - The response from the Fresh server.
+ */
 export default async function handler(req) {
 	const server = await import("../_fresh/server.js");
-	server.default.fetch(req);
+
+	return server.default.fetch(req);
 };
