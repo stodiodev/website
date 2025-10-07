9d3e6f7f4f46b3dcab1e6c417bf094a705a7d6c1
Savin Angel-Mario <hello@m4.rocks>
2025-10-07 17:41:29 +0000

    Another test

--- vercel-endpoint/handler.js
+++ vercel-endpoint/handler.js
@@ -5,5 +5,14 @@
 export default async function handler(req) {
 	const server = await import("../_fresh/server.js");
 
-	return server.default.fetch(req);
+	const url = new URL(req.url ?? '', `https://${req.headers.host}`);
+
+	const standardRequest = new Request(url.toString(), {
+		method: req.method,
+		headers: new Headers(req.headers),
+		body: ['GET', 'HEAD'].includes(req.method ?? '') ? undefined : JSON.stringify(req.body),
+	});
+
+
+	return server.default.fetch(standardRequest);
 };
